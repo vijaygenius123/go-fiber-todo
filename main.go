9029cd9bf59c5e8d1bfcade35b7015287213a101
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2022-03-25 08:34:24 +0000

    Add Todo struct with GET and POST methods

--- main.go
+++ main.go
@@ -5,14 +5,43 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Starting")
 
 	app := fiber.New()
 
+	var todos = []Todo{}
+
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
 	})
 
-	app.Listen(":8080")
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+	})
+
+	err := app.Listen(":8080")
+	if err != nil {
+		fmt.Println("Error Starting Server")
+	}
 }
